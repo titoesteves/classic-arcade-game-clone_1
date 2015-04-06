1dc4dce08ed48c20c7e34b1f883a2691d4ef8c75
titoesteves <titoesteves@users.noreply.github.com>
2015-04-05 20:16:11 -0400

    Committed by Udacity

---
+++ frontend-nanodegree-arcade-game/js/app.js.old.js
@@ -0,0 +1,91 @@
+// Enemies our player must avoid
+var Enemy = function(x,y,s) {
+    // Variables applied to each of our instances go here,
+    // we've provided one for you to get started
+    this.x = x;
+    this.y = y;
+    this.speed = s;
+    // The image/sprite for our enemies, this uses
+    // a helper we've provided to easily load images
+    this.sprite = 'images/enemy-bug.png';
+}
+
+// Update the enemy's position, required method for game
+// Parameter: dt, a time delta between ticks
+Enemy.prototype.update = function(dt) {
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+    this.x += this.speed * dt;
+
+}
+
+// Draw the enemy on the screen, required method for game
+Enemy.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
+
+// Now write your own player class
+// This class requires an update(), render() and
+// a handleInput() method.
+    var Player = function(x, y, s){
+        this.x = x;
+        this.y = y;
+        this.speed = s;
+        this.sprite = 'images/char-boy.png';
+        this.handleInput = function(key){
+            if (key === "left" && this.x > 0){
+                this.x-= 100;
+            }
+            else if (key === "right" && this.x < 355){
+                this.x+= 100;
+            }
+            else if (key === "up" && this.y > 55){
+                this.y-= 85;
+            }
+            else if (key === "down" && this.y < 400){
+                this.y+= 85;
+            }
+        //     else if (this.y < 25){
+        //         player.x = 200, player.y = 400;
+        //         player.reset();
+        //         render();
+        // }
+        };
+    };
+
+    Player.prototype.update = function(){
+        if(this.y < 25){
+            player.reset();
+        }
+    };
+
+// Draw the player on the screen, required method for game
+        Player.prototype.render = function() {
+            ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+        }
+
+// Now instantiate your objects.
+// Place all enemy objects in an array called allEnemies
+// Place the player object in a variable called player
+    var allEnemies = [];
+    var i = 0, x = 0, y = 60, s = 300;
+    for(i ; i < 3; i++, x+=180, y+= 85, s+=100){
+        allEnemies.push(new Enemy(x, y, s));
+    }
+
+    var player = new Player(200, 400, 200);
+
+
+// This listens for key presses and sends the keys to your
+// Player.handleInput() method. You don't need to modify this.
+document.addEventListener('keyup', function(e) {
+    var allowedKeys = {
+        37: 'left',
+        38: 'up',
+        39: 'right',
+        40: 'down'
+    };
+
+    player.handleInput(allowedKeys[e.keyCode]);
+});
